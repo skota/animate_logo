4fe9566a8e3b549b0834f5f182682b766dd7ae85
sriram kota <sriramkota@yahoo.com>
2020-01-28 14:11:51 -0500

    initial checkin

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+void main() => runApp(AnimationPage());
+
+class AnimationPage extends StatefulWidget {
+  @override
+  _AnimationPageState createState() => _AnimationPageState();
+}
+
+class _AnimationPageState extends State<AnimationPage>
+    with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController animCtrl;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animCtrl = AnimationController(
+      duration: Duration(seconds: 5),
+      vsync: this,
+    );
+
+    //now setup a curve
+    final curvedAnimation = CurvedAnimation(
+      parent: animCtrl,
+      curve: Curves.bounceIn,
+      reverseCurve: Curves.easeOut,
+    );
+
+    //setup tween- range of values
+    // use tween to animate animCtrl
+    //animation = Tween<double>(begin: 0, end: 2 * math.pi).animate(animCtrl);
+
+    // use tween to animate curves -
+    animation = Tween<double>(begin: 150, end: 300).animate(curvedAnimation);
+
+    //setstate is the listener..when animation values change --rebuild
+    animCtrl.addListener(() {
+      setState(() {});
+    });
+
+    //we also want to listen to when the animation status changes
+    // status cane be - dismissed, forward, completed, reversed
+    animCtrl.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        animCtrl.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        animCtrl.forward();
+      }
+    });
+
+    //start the animation
+    animCtrl.forward();
+  }
+
+  @override
+  void dispose() {
+    animCtrl.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    color: Colors.grey[200],
+                    width: animation.value,
+                    height: animation.value,
+                    child: Image.asset("images/logo.png"),
+                  ),
+                  SizedBox(height: 50),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
